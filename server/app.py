8e6e877b21770a853fa6ce51820c6d9fefc53dfa
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-09-28 21:05:35 +0530

    Added interface to get execute code and send result

--- server/app.py
+++ server/app.py
@@ -1,16 +1,44 @@
-#!/usr/bin/python3
+#!../flask/bin/python3
 
+# import sys
 from flask import Flask, jsonify, abort, request, make_response, url_for
+
+def executeCode(inp):
+	# --------------------------------------------------------------------------
+	# Put your code to connect to a docker container.
+	# --------------------------------------------------------------------------
+
+
+	# --------------------------------------------------------------------------
+	# Return a dictionary. 
+	# Following is just a representation with some suggested keys.
+	# Your dictionary can contain any result you deem important
+	# for an user to see after he's submitted the code. 
+	# --------------------------------------------------------------------------
+	return {
+		"errors":"",
+		"warnings":"",
+		"numOfTestCasesPassed":0,
+		"testCasesPassed":[]
+	}
 
 app = Flask(__name__, static_url_path = "")
     
 @app.errorhandler(400)
 def notFound(error):
-    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+    return make_response(
+    	jsonify({ 
+    		'error': 'Bad request' 
+    		}), 
+    	400)
 
 @app.errorhandler(404)
 def notFound(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    return make_response(
+    	jsonify({ 
+    		'error': 'Not found' 
+    		}), 
+    	404)
 
 @app.route('/submitCode/<int:USN>', methods = ['GET'])
 def getInfo(USN):
@@ -21,7 +49,17 @@ def getInfo(USN):
 def getCode():
     if(not request.json):
         abort(400)
-    return (jsonify( { 'task': request.json } ), 201)
+    inputJson = request.json
+    # print(res["code"],file=sys.stderr)
+    
+    output = executeCode(inputJson)
+    
+    outputJson = jsonify({
+    		"input":inputJson,
+    		"output":output
+    	})
+
+    return (outputJson, 201)
 
 if __name__ == '__main__':
     app.run(debug = True)
--- server/run.py
+++ server/run.py
@@ -1,40 +1,54 @@
-#!/usr/bin/python3
+#!../flask/bin/python3
 
 import requests as req
 import argparse
 
-def sendCode():
+def sendCode(inputCode,questionHash):
 	# Open the code file using the path sent.
-	fp = open(args.inputCode,"r")
 	
 	code = None
 
-	if(fp.readable()):
-		code = fp.readlines()
+	with open(inputCode,"r") as fp:
+		if(fp.readable()):
+			code = fp.read()
 
 	# Create the data object to be sent to server.
 	data = {
-		"USN":"01FB15ECS889",
+		"USN":[],
 		"code":code,
 		"progLang":"Python3",
-		"questionHash":args.questionHash,
+		"questionHash":questionHash,
 	}
 
 	# Make a post request with the data to be sent.
-	res = req.post("http://localhost:5000/submitCode",json = data,headers={"content-type":"application/json"})
+	res = req.post("http://localhost:5000/submitCode",
+			json = data,
+			headers={
+				"content-type":"application/json"
+			}
+		)
 
+	print("res")
 	if(res.ok):
 		return(res.text)
 	else:
 		return("Error occured : ",res.status_code)
 
 
-if __name__=="main":
+if __name__=="__main__":
 	# Parse CLI Arguments
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("-u","--upload",dest='inputCode', type=str, required=True, help="/path/to/the/file/having/code.ext")
-	parser.add_argument("-q","--question",dest='questionHash',  type=str, help="Hash value of the question")
+	parser.add_argument("-u","--upload",
+		dest='inputCode', 
+		type=str, 
+		required=True, 
+		help="/path/to/the/file/having/code.ext")
+
+	parser.add_argument("-q","--question",
+		dest='questionHash', 
+		type=str, 
+		help="Hash value of the question")
 
 	args = parser.parse_args()
 
