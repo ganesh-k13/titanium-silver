4460b7cf9615423ab6e29f01f07177479d7398ff
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-03-23 23:07:00 +0530

    Homepage Navbar added

---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Navbar} from "react-bootstrap";
+import "../../../node_modules/font-awesome/css/font-awesome.min.css";
+
+function Header() {
+    return (
+        <Navbar bg="dark" variant="dark" style={navBarStyle}>
+            <Navbar.Brand><i className="fa fa-align-justify"></i>&nbsp;&nbsp;&nbsp;Titanium Silver</Navbar.Brand>
+        </Navbar>
+    );
+}
+
+const navBarStyle = {
+    position: "fixed",
+    top: "0px",
+    left: "0px",
+    zIndex: "998",
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    color: "#000000"
+}
+
+export default Header;
