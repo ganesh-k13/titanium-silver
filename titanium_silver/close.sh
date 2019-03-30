188318109ed606238b10b5b29f198df3ce8d5650
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-03-30 22:10:23 +0530

    Changed close.sh

--- titanium_silver/close.sh
+++ titanium_silver/close.sh
@@ -1,8 +1,17 @@
+# #!/bin/bash
+# # Basic while loop
+# counter=0
+# while [ $counter -le $2 ]
+# do
+# 	sudo docker rm $1$counter
+# 	((counter++))
+# done
+
 #!/bin/bash
 # Basic while loop
 counter=0
 while [ $counter -le $2 ]
 do
-	sudo docker rm $1$counter
+	cp $2 "$3-$counter.cpp"
 	((counter++))
 done
