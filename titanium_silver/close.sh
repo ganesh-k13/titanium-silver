c340946831ce7d3dd20b34a6648043980373b2d5
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-03-31 10:18:52 +0530

    Deleted clone.sh and recloned it from master via wget

--- titanium_silver/close.sh
+++ titanium_silver/close.sh
@@ -1,17 +1,8 @@
-# #!/bin/bash
-# # Basic while loop
-# counter=0
-# while [ $counter -le $2 ]
-# do
-# 	sudo docker rm $1$counter
-# 	((counter++))
-# done
-
 #!/bin/bash
 # Basic while loop
 counter=0
 while [ $counter -le $2 ]
 do
-	cp $2 "$3-$counter.cpp"
+	sudo docker rm $1$counter
 	((counter++))
 done
