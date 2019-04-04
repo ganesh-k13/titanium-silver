03b194a2f861e10ee4245c1a69c69d1321c7b41b
Ganesh Kathiresan <ganesh3597@gmail.com>
2019-04-04 17:31:06 +0530

    Added new languages tests

---
+++ tests/SC/myapp.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int args, char *argv[]) {
+	printf("Hello container: %s", argv[1]);
+	return 0;
+}
+
---
+++ tests/test_c.py
@@ -0,0 +1,19 @@
+import sys
+import pytest
+from titanium_silver.docker_client import Docker_Client
+from threading import Thread, Event
+import time
+import os
+import pdb
+
+@pytest.mark.skipif(sys.platform == 'darwin', reason="does not run on osx yet")
+def test_basic():
+	dcli = Docker_Client()
+	# print(os.getcwd())
+	thread_list = list()
+	for i in range(1):
+		print("Spawn container: %d"%i)
+		thread_list.append(dcli.spawn_process(name='myapp', num=i, params='%d 5000'%i, path=os.getcwd()+'/tests/SC', lang='CContainer'))
+	
+	for i, t in enumerate(thread_list):
+		assert('Hello container: %d'%i == t.result_queue.get().decode('utf-8'))
