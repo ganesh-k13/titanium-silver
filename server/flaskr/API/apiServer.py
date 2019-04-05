73f8d42158689d916d41b9a5052bb8b5dec00a7f
Ganesh Kathiresan <ganesh3597@gmail.com>
2019-04-05 14:11:45 +0530

    Better Ineritance | C

--- server/flaskr/API/apiServer.py
+++ server/flaskr/API/apiServer.py
@@ -4,15 +4,15 @@ import random
 from titanium_silver.docker_client import Docker_Client
 from server.flaskr import app
 
-EXTENSIONS = {
-    "C++":"cpp",
-    "Python":"py",
-    "Python3":"py",
-    "C":"c",
-    "Ruby":"rb",
-    "PHP5.x":"php",
-    "PHP7.x":"php",
-    "Java":"java"
+META_DATA = {
+    "C++":{"extension": "cpp", "container":"CppContainer"},
+    "Python":{"extension": "py", "container":"Python2Container"},
+    "Python3":{"extension": "py","container":"PythonContainer"},
+    "C":{"extension": "c","container":"CContainer"},
+    "Ruby":{"extension": "rb","container":"RubyContainer"},
+    "PHP5.x":{"extension": "php","container":"Php5Container"},
+    "PHP7.x":{"extension": "php","container":"Php7Container"},
+    "Java":{"extension": "java","container":"JavaContainer"}
 }
 
 def uploadCode(inputJson):
@@ -24,7 +24,7 @@ def uploadCode(inputJson):
     file_name = inputJson['USN']+"_"+inputJson['questionHash']
     inpFileName = os.path.join(
         app.config["INPUT_FOLDER"],
-        file_name+"."+EXTENSIONS[inputJson["progLang"]]
+        file_name+"."+META_DATA[inputJson["progLang"]]["extension"]
     )
 
     # Open the file in 'w' mode to either overwrite previous
@@ -49,11 +49,11 @@ def uploadCode(inputJson):
     # path must be relative to INPUT_FOLDER.
     # For example, if INPUT_FOLDER is '/server/flaskr/codes/Input' and TC is like: 
     # input: /server/flaskr/codes/Input/TC_IN/q1_1.in, q1_2.in and output: /server/flaskr/codes/Input/TC_OUT/q1_1.out, q1_2.out
-	# Variables must be: tc_in = [TC_IN/q1_1.in, TC_IN/q1_2.in] and tc_out = [/TC_OUT/q1_1.out, /TC_OUT/q1_2.out]
+    # Variables must be: tc_in = [TC_IN/q1_1.in, TC_IN/q1_2.in] and tc_out = [/TC_OUT/q1_1.out, /TC_OUT/q1_2.out]
 
     random.seed(inputJson['USN'])
     dcli = Docker_Client()
-    thread = dcli.spawn_process(name=file_name, num=random.randint(1, 99999999), params='%s 5000'%inputJson['USN'], path=app.config["INPUT_FOLDER"])
+    thread = dcli.spawn_process(name=file_name, num=random.randint(1, 99999999), params='%s 5000'%inputJson['USN'], path=app.config["INPUT_FOLDER"], lang=META_DATA[inputJson["progLang"]]["container"])
     # Create a filename: 
     # opFileName = 
     #    "op" + _ + USN_of_user + _ + questionHash
