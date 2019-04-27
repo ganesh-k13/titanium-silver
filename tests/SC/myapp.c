db8791dd444913e1a4eed9300ebbf1ba4433e383
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-04-27 18:19:03 +0530

    Removed waste files

--- tests/SC/myapp.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int args, char *argv[]) {
-	printf("Hello container: %s", argv[1]);
-	return 0;
-}
-
--- tests/SC/myapp.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-print("Hello container: %s"%sys.argv[1])
