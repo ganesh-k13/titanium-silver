4ce0e1c56014ba04e0c6cf32f95d5f920b9b3c81
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-04-27 20:27:21 +0530

    Added back a deleted files to fix PR

---
+++ tests/SC/out/usn-22.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+#include <string>
+
+int main(int args, char *argv[]) {
+	// std::this_thread::sleep_for(std::chrono::milliseconds(atoi(argv[2])));
+	std::string to_print = "Hello container: ";
+	to_print.append(argv[1]);
+	std::cout << to_print;
+	return 0;
+}
+
