fec3cf7637e3aae03dc77d33d824da0e76b76da9
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-05-17 22:31:04 +0530

    Trying production in travis.yml

--- dbCreator.py
+++ dbCreator.py
@@ -2,7 +2,7 @@ from server.flaskr import db
 from server.config import DevelopmentConfig
 import sqlalchemy
 
-engine = sqlalchemy.create_engine("mysql+pymysql://root:RahulRB@1997@localhost") # mysql+pymysql://user:password@host
+engine = sqlalchemy.create_engine("mysql+pymysql://root:@127.0.0.1") # mysql+pymysql://user:password@host
 engine.execute("CREATE DATABASE TitaniumSilver") #create db
 
 db.create_all()
