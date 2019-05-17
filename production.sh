d0d979b3db1001fdc7597c02928738cdcca4cb51
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-05-17 20:40:18 +0530

    Added a helper message in production script

--- production.sh
+++ production.sh
@@ -19,8 +19,17 @@ bash --rcfile "${VENV}/bin/activate" -ci "
 	echo \"----Creating Databases----\"
 	cd ../../
 	python3 dbCreator.py
-	
+
+	echo \"\"
+	echo \"\"
+	echo \"\"
+	echo \"----------------------------------------------------------------------------------------\"
+	echo \"Look at ./sample-questions.zip for some examples of how to form questions and test cases\"
+	echo \"----------------------------------------------------------------------------------------\"
+	echo \"\"
+	echo \"\"
+	echo \"\"
+
 	echo \"----Starting Gunicorn Processes---\"
 	gunicorn --bind 0.0.0.0:8000 --workers=4 run:app
 "
-
