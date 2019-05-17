f694ad57187cb54dbb08c193ace19a926a7b9b1d
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-05-17 21:26:55 +0530

    Added production instructions to README.md

--- dbCreator.py
+++ dbCreator.py
@@ -1,3 +1,8 @@
 from server.flaskr import db
+from server.config import DevelopmentConfig
+import sqlalchemy
+
+engine = sqlalchemy.create_engine("mysql+pymysql://root:RahulRB@1997@localhost") # mysql+pymysql://user:password@host
+engine.execute("CREATE DATABASE TitaniumSilver") #create db
 
 db.create_all()
